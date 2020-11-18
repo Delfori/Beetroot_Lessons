7f93f94c6173d28a0a57fb41a3fe7abb0aae7497
Delfori <sergioknut@gmail.com>
2020-11-19 00:07:39 +0200

    'Homework Lesson 8'

--- Homeworks/homework_lesson_8.py
+++ Homeworks/homework_lesson_8.py
@@ -1,1 +1,32 @@
-print('Test homework for git')
+# Task 1
+def oops():
+    raise IndexError
+
+
+def function_that_calls_oops():
+    try:
+        oops()
+    except IndexError:
+        print('ошибка IndexError')
+    except KeyError:
+        print('ошибка KeyError')
+
+
+function_that_calls_oops()
+
+# Task 2
+
+
+def function_that_takes_in_two_numbers(a=input('number 1?\n'), b=input('number 2?\n')):
+    try:
+        a = int(a)
+        b = int(b)
+        result = a * a / b
+        return result
+    except ZeroDivisionError:
+        print('cannot divide by zero')
+    except TypeError:
+        print('wrong type')
+
+
+function_that_takes_in_two_numbers()
